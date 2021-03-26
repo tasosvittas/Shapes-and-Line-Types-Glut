9d8cae6e50977f21b7390da0db7a5f485ac6af3a
tasosvittas <tasosvittas@gmail.com>
2021-03-26 09:10:44 +0200

    Initial Commit

---
+++ MenuExercise/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MenuExercise
+//
+//  Created by Tasos Vittas on 26/3/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
